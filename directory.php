fdab1088280de70fab62bb20f939aa4ea8488740
nugget <nugget>
2004-10-20 04:34:13 +0000

    Moo.

---
+++ directory.php
@@ -0,0 +1,21 @@
+<CiscoIPPhoneDirectory>
+  <Title>Slacker Phone Numbers</Title>
+  <Prompt>People</Prompt>
+  <DirectoryEntry>
+    <Name>Tom</Name>
+    <Telephone>tomn@sneaky.net</Telephone>
+  </DirectoryEntry>
+  <DirectoryEntry>
+    <Name>Mom</Name>
+    <Telephone>12178962082</Telephone>
+  </DirectoryEntry>
+  <DirectoryEntry>
+    <Name>Len in California</Name>
+    <Telephone>13104594291</Telephone>
+  </DirectoryEntry>
+  <DirectoryEntry>
+    <Name>Len in Idaho</Name>
+    <Telephone>12087886728</Telephone>
+  </DirectoryEntry>
+</CiscoIPPhoneDirectory> 
+
