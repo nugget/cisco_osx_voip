25d8d68c4fb8fb60c62ba29af0faec5971168c73
nugget <nugget>
2004-12-18 23:35:16 +0000

    Moo.

--- directory.php
+++ directory.php
@@ -7,23 +7,16 @@
   <Title>Slacker Phone Numbers</Title>
   <Prompt>People</Prompt>
   <DirectoryEntry>
-    <Name>Weather</Name>
-    <Telephone>3172222222</Telephone>
+    <Name>Indy Weather</Name>
+    <Telephone>+1 317 222-2222</Telephone>
+  </DirectoryEntry>
+  <DirectoryEntry>
+    <Name>RaYmAn</Name>
+    <Telephone>nugget@kp.slacker.com</Telephone>
   </DirectoryEntry>
   <DirectoryEntry>
     <Name>Tom</Name>
     <Telephone>tomn@sneaky.net</Telephone>
   </DirectoryEntry>
-  <DirectoryEntry>
-    <Name>Mom</Name>
-    <Telephone>12178962082</Telephone>
-  </DirectoryEntry>
-  <DirectoryEntry>
-    <Name>Len in California</Name>
-    <Telephone>13104594291</Telephone>
-  </DirectoryEntry>
-  <DirectoryEntry>
-    <Name>Len in Idaho</Name>
-    <Telephone>12087886728</Telephone>
-  </DirectoryEntry>
 </CiscoIPPhoneDirectory> 
+
--- services.php
+++ services.php
@@ -2,14 +2,11 @@
  header("Content-type: text/xml");
  header("Connection: close");
  header("Expires: -1");
-?>
-<CiscoIPPhoneMenu>
-  <Title>slacker.com services</Title>
-  <Prompt>Choose a service</Prompt>
-
-  <MenuItem>
-    <Name> Order Some Food</Name>
-    <URL>http://slacker.com/cisc0/food.php</URL>
-  </MenuItem>
-
+?><CiscoIPPhoneMenu>
+<Title>slacker.com services</Title>
+<Prompt>Choose a service</Prompt>
+<MenuItem>
+<Name> Food Delivery</Name>
+<URL>http://slacker.com/cisc0/food.php</URL>
+</MenuItem>
 </CiscoIPPhoneMenu>
